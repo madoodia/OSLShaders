4c0fd0e8a160e2a00e1e323c564c01715cb9a365
Reza Aarabi <madoodia@gmail.com>
2020-11-02 15:21:48 -0800

    a few try more on creating osl shaders

--- src/core/core.h
+++ src/core/core.h
@@ -1,11 +1,20 @@
-/* --------------------- */
-/* (C) 2020 madoodia.com */
-/*  All Rights Reserved  */
-/* --------------------- */
+// /* --------------------- */
+// /* (C) 2020 madoodia.com */
+// /*  All Rights Reserved  */
+// /* --------------------- */
 
-#ifndef MYCORE_H
-#define MYCORE_H
+// #ifndef MYCORE_H
+// #define MYCORE_H
 
-float myadd(float a, float b) { return a + b; }
+// float myadd(float a, float b) { return a + b; }
 
-#endif // MYCORE_H
+// displacement bumpy(float bumpdist = 1,
+//                    string bumpunits = "cm")
+// {
+//   // convert bumpdist to common units
+//   float spacing = transformu(bumpunits, "common", bumpdist);
+//   float n = noise(P / spacing);
+//   displace(n);
+// }
+
+// #endif // MYCORE_H
